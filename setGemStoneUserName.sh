849235078850e2d619971c50d4d78aeca46adda4
Paul DeBruicker <pdebruic@gmail.com>
2016-12-07 07:45:35 -0800

    swapped out references to defSeaside with those that work with GsDevKit_home
    edited the setGemStoneUserName.sh to also set the name of the stone and net ldi port.
    
    after cloning this repo edit that script and run it.

--- setGemStoneUserName.sh
+++ setGemStoneUserName.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-find . -type f -name "run" -exec sed -i'' -e 's/DAEMONTOOLS_USER/THE_NAME_OF_THE_USER_YOU_WANT_GEMSTONE_TO_RUN_UNDER/g' {} +
+STONE_USER=glass
+STONE_NAME=seaside
+NETLDI_PORT=50377
+
+find . -type f -name "run" -exec sed -i'' -e "s/DAEMONTOOLS_USER/$STONE_USER/g" {} +
+find . -type f -name "run" -exec sed -i'' -e "s/YOUR_STONE_NAME/$STONE_NAME/g" {} +
+find . -type f -name "run" -exec sed -i'' -e "s/YOUR_NET_LDI_PORT/$NETLDI_PORT/g" {} +
